ad5c4ec150d60aadd019d70410cd4441f7acf490
Alan Cristhian <alan.cristh@gmail.com>
2014-01-09 21:54:23 -0300

    first upload

---
+++ applications/helpers/jquery-sizeloaded.js
@@ -0,0 +1,28 @@
+;(function($, undefined) {
+    "use strict";
+
+
+    $.fn.sizeloaded = function(options) {
+        var defaults = {
+                delay: 33
+            },
+            settings = $.extend({}, defaults, options);
+
+
+        return this.each(function () {
+            var checker = setInterval(_check_dimensions,
+                    settings.delay),
+                $this = $(this),
+                image = $this[0];
+
+            function _check_dimensions() {
+                if (image.naturalWidth !== 0
+                && image.naturalHeight !== 0
+                || image.complete) {
+                    $this.trigger('sizeloaded');
+                    clearInterval(checker);
+                }
+            }
+        });
+    };
+})(jQuery);
