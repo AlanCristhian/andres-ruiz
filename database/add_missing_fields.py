b0cbe08bc03f3ac2dd641aa10a788719c30fba98
Alan Cristhian <alan.cristh@gmail.com>
2014-03-08 16:10:43 -0300

    create a script that add more fields for each table and create the multimedia and contact_info tables

---
+++ database/add_missing_fields.py
@@ -0,0 +1,248 @@
+import shutil
+import os
+
+from framework import servermodel
+
+# TODO:
+# - Remove the location table.
+# - Remove the images table.
+# - Remove the videos table.
+# - Remove the location table.
+
+# Create a copy of models.db
+shutil.copyfile('database/models.db', 'models_copy.db')
+shutil.copystat('database/models.db', 'models_copy.db')
+
+
+class AddMissingData:
+    def __init__(self):
+        assert os.path.lexists('models_copy.db'), \
+            'you must create a copy of models.db called models_copy.db'
+        self.collection = servermodel.Collection(dbpath='models_copy.db');
+
+    def __enter__(self): 
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def add_contact_info_table(self):
+        """Create the *contact_info* table."""
+        self.collection.create(name='contact_info', fields={
+            'id':              'prim',
+            'service_name':    'str',
+            'service_address': 'str',
+            'user_name':       'str',
+            'data':            'str',
+        })
+        self.collection.bind(
+            bindName='contact_info_to_users',
+            modelSource='contact_info',
+            fieldSource='user_name',
+            modelTarget='users',
+            fieldTarget='user_name',
+        )
+
+        # get the contact_info table
+        self.contact_info = self.collection.get('contact_info')
+        self.contact = self.collection.get('contact')
+
+    def insert_contact_info_data(self):
+        # Get the user_name register.
+        try:
+            _users = self.contact.get(fields='user_name')
+            print(self.contact.get('*'))
+        except IndexError as e:
+            raise e
+
+        for _user_name in _users:
+            # Set the address register
+            try:
+                _address = self.contact.get(
+                    fields='address',
+                    where='user_name=?',
+                    params=_user_name,
+                    format='strList',
+                )[0]
+            except IndexError:
+                _address = ''
+            finally:
+                print(_address)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='address',
+                    service_address=_address,
+                )
+                '''
+
+            # Set the email register
+            try:
+                _email = self.contact.get(
+                    fields='email',
+                    format='strList',
+                )[0]
+            except IndexError:
+                _email = ''
+            finally:
+                print(_email)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='email',
+                    service_address=_email,
+                )
+                '''
+
+            # Set the facebook register.
+            try:
+                _facebook = self.contact.get(
+                    fields='facebook',
+                    format='strList',
+                )[0]
+            except IndexError:
+                _facebook = ''
+            finally:
+                print(_facebook)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='facebook',
+                    service_address=_facebook,
+                )
+                '''
+
+            # Set the twitter register.
+            try:
+                _twitter = self.contact.get(
+                    fields='twitter',
+                    format='strList'
+                )[0]
+            except IndexError:
+                _twitter = ''
+            finally:
+                print(_twitter)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='twitter',
+                    service_address=_twitter,
+                )
+                '''
+
+            # Set the pinterest register.
+            try:
+                _pinterest = self.contact.get(
+                    fields='pinterest',
+                    format='strList',
+                )[0]
+            except IndexError:
+                _pinterest = ''
+            finally:
+                print(_pinterest)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='pinterest',
+                    service_address=_pinterest,
+                )
+                '''
+
+            # Set the telephone register
+            try:
+                _telephone = self.contact.get(
+                    fields='telephone',
+                    format='strList',
+                )[0]
+            except IndexError:
+                _telephone = ''
+            finally:
+                print(_telephone)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='telephone',
+                    service_address=_telephone,
+                )
+                '''
+
+            # Set the mobile register.
+            try:
+                _mobile = self.contact.get(
+                    fields='mobile',
+                    format='strList'
+                )[0]
+            except:
+                _mobile = ''
+            finally:
+                print(_mobile)
+                '''
+                self.contact_info.insert(
+                    user_name=_user_name,
+                    service_name='mobile',
+                    service_address=_mobile,
+                )
+                '''
+
+    def add_multimedia_table(self):
+        """Create the *multimedia* table."""
+        self.collection.create(name='multimedia', fields={
+            'id':              'intPrim',
+            'creation_date':   'datetime',
+            'last_modified':   'datetime',
+            'url':             'str',
+            'description':     'str',
+            'classification':  'str',
+            'article_name':    'str',
+            'position':        'int',
+            'acceptance':      'float',
+            'zoomed':          'int',
+            'likes':           'int',
+            'dislikes':        'int',
+            'data':            'str',
+            'cover':           'bool',
+            'type':            'str',
+        })
+        self.collection.bind(
+            bindName='multimedia_to_articles',
+            modelSource='multimedia',
+            fieldSource='article_name',
+            modelTarget='articles',
+            fieldTarget='article_name',
+        )
+        self.multimedia = self.collection.get('multimedia')
+        self.images = self.collection.get('images')
+
+    def add_multimedia_data(self):
+        _images = self.images.get(
+            fields='*',
+            format='dictList'
+        )
+        for _image in _images:
+            _multimedia = _image
+            _multimedia.update({'type': 'image_file'})
+            self.multimedia.insert(**_multimedia)
+
+    def add_missing_fields(self):
+        _fields = {'data': 'str'}
+        self.collection.add_fields(name='articles', fields=_fields)
+        self.collection.add_fields(name='comments', fields=_fields)
+        self.collection.add_fields(name='visitors', fields=_fields)
+        self.collection.add_fields(name='users', fields=_fields)
+
+if __name__ == '__main__':
+    add_missing_data = AddMissingData()
+    '''
+    add_missing_data.add_contact_info_table()
+    add_missing_data.insert_contact_info_data()
+    add_missing_data.add_multimedia_table()
+    add_missing_data.add_multimedia_data()
+    add_missing_data.add_missing_fields()
+    '''
+    with add_missing_data as _:
+        _.add_contact_info_table()
+        _.insert_contact_info_data()
+        _.add_multimedia_table()
+        _.add_multimedia_data()
+        _.add_missing_fields()
