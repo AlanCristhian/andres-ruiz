d88916bd81125ba3a560f2e2f00813864d3e5462
Alan Cristhian <alan.cristh@gmail.com>
2014-03-20 10:44:27 -0300

    replace the images table by multimedia.

--- applications/editArticle/controllers/get_article_data.py
+++ applications/editArticle/controllers/get_article_data.py
@@ -1,4 +1,8 @@
+from urllib import parse
+
 from framework import core
+from framework import security
+
 from applications.helpers import helpers
 
 
@@ -6,7 +10,9 @@ class GetData(metaclass=core.Main):
 
     def setUp(self):
         self.articles = self.serverCollection.get('articles')
-        self.articleURL = self.clientModel.form.get('edit_url')
+        url = self.clientModel.form.get('edit_url')
+        # remove initial slash
+        self.articleURL = url = url[1:] if url[0] == '/' else url
         self.dataGroup = self.clientModel.form.get('data_group')
         self.response.set_expires(0)
         self.response.set_content_type('application/json')
@@ -29,35 +35,53 @@ class GetData(metaclass=core.Main):
             # Remove the bar "/" and add the shared ssl name if is an https
             # protocol and the sharedSSL variable is enabled in the
             # applications/settings.py config file.
-            ,params=helpers.remove_user_name(self.articleURL[1:])
+            ,params=helpers.remove_user_name(self.articleURL)
             ,format='dictList')
 
     def get_article_description(self):
         return self.articles.get(
             fields=('id', 'description')
             ,where='edit_url=?'
-            ,params=helpers.remove_user_name(self.articleURL[1:])
+            ,params=helpers.remove_user_name(self.articleURL)
             ,format='dictList')
 
     def get_artile_cover(self):
         return self.articles.get(
             fields=('id', 'cover_image', 'cover_description')
             ,where='edit_url=?'
-            ,params=helpers.remove_user_name(self.articleURL[1:])
+            ,params=helpers.remove_user_name(self.articleURL)
             ,format='dictList')
 
-    def get_article_images(self):
+    def get_article_multimedia(self):
         self.articleName = self.articles.get(
             fields='article_name'
             ,where='edit_url=?'
-            ,params=helpers.remove_user_name(self.articleURL[1:])
+            ,params=helpers.remove_user_name(self.articleURL)
             ,distinct=True)
-        self.images = self.serverCollection.get('images')
-        return [self.images.get(
-            fields=('id', 'url', 'description', 'classification')
+        self.multimedia = self.serverCollection.get('multimedia')
+        _database_result = self.multimedia.get(
+            fields=('id', 'url', 'description', 'type', 'cover', 'article_name')
             ,where='article_name=?'
             ,params=self.articleName[0]
-            ,format='dictList')]
+            ,format='dictList')
+        result = []
+        for item in _database_result:
+            # add the hostname and the protocol to the url if is an file path
+            if item['type'] == 'image_file':
+                item['url'] = security.set_absolute_path(item['url'])
+
+            # add the video id key if is an youtube video
+            elif item['type'] == 'video_link':
+                url_tuple = parse.urlparse(item['url'])
+                queries = parse.parse_qs(url_tuple.query)
+                # check if the url have the 'v' key
+                if queries.get('v'):
+                    item['vid'] = queries['v'][0]
+                else:
+                    item['vid'] = ''
+            result.append(item)
+
+        return [result]
 
     def handler(self):
         if self.dataGroup == 'info':
@@ -66,5 +90,5 @@ class GetData(metaclass=core.Main):
             self.get_data(self.get_article_description)
         if self.dataGroup == 'cover':
             self.get_data(self.get_artile_cover)
-        if self.dataGroup == 'images':
-            self.get_data(self.get_article_images)
+        if self.dataGroup == 'multimedia':
+            self.get_data(self.get_article_multimedia)
