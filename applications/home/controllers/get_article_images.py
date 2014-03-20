9297a0048bed66d88b90cb7d2b307e479a5d924c
Alan Cristhian <alan.cristh@gmail.com>
2014-03-20 11:11:20 -0300

    nose extract the data from the collection table

--- applications/home/controllers/get_article_images.py
+++ applications/home/controllers/get_article_images.py
@@ -1,45 +1,58 @@
+from urllib import parse
+import os
+
 from framework import core
+from framework import security
+
+from applications.helpers import helpers
 
 
 class GetArticleModel(metaclass=core.Main):
     """Response a JSON with all info of the article.
     """
     def setUp(self):
-        self.article = self.serverCollection.get('articles')
-        self.images = self.serverCollection.get('images')
-        self.get_article_name()
+        # get the url of the article
+        url = os.environ\
+            .get('REQUEST_URI')\
+            .replace('images/', '')
+        self.article_URL = url[1:] if url[0] == '/' else url
 
-
-    def get_article_name(self):
-        """Return the name of the article binded to the actual url.
-        """
-        # get the article url
-        if hasattr(self.clientModel, 'https') \
-        and self.config.enableSharedSSL:
-            self.article_URL = self.clientModel.url.replace('/~andresru/images/', '')
-        else:
-            self.article_URL = self.clientModel.url.replace('/images/', '')
-
-        # get fields
-        return self.article.get(
+        self.articles = self.serverCollection.get('articles')
+        self.multimedia = self.serverCollection.get('multimedia')
+        self.article_name = self.articles.get(
             fields='article_name'
             ,where='url=?'
-            ,params=self.article_URL
+            ,params=helpers.remove_user_name(self.article_URL)
             ,distinct=True)
 
-    def get_images(self, url=None):
-        """Return a list with image info.
-        """
-        return self.images.get(
-            fields=('id', 'url', 'article_name', 'description')
+    def get_multimedia(self):
+        _database_result = self.multimedia.get(
+            fields=('id', 'url', 'description', 'type', 'cover', 'article_name')
             ,where='article_name=?'
-            ,params=url
+            ,params=self.article_name[0]
             ,format='dictList')
+        result = []
+        for item in _database_result:
+            # add the hostname and the protocol to the url if is an file path
+            if item['type'] == 'image_file':
+                item['url'] = security.set_absolute_path(item['url'])
+
+            # add the video id key if is an youtube video
+            elif item['type'] == 'video_link':
+                url_tuple = parse.urlparse(item['url'])
+                queries = parse.parse_qs(url_tuple.query)
+                # check if the url have the 'v' key
+                if queries.get('v'):
+                    item['vid'] = queries['v'][0]
+                else:
+                    item['vid'] = ''
+            result.append(item)
+
+        return result
 
     def handler(self):
-        _article_name = self.get_article_name()
-        if _article_name:
-            _content = self.get_images(_article_name[0][0])
+        if self.article_name:
+            _content = self.get_multimedia()
             self.basic_response(
                 content=_content,
                 contentType='application/json',
