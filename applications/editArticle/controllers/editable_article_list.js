1310cda84efb8c1a7d7c3404963dd32af47c82ae
Alan Cristhian <alan.cristh@gmail.com>
2014-03-20 10:43:02 -0300

    now the template is choise between many templates

--- applications/editArticle/controllers/editable_article_list.js
+++ applications/editArticle/controllers/editable_article_list.js
@@ -1,8 +1,8 @@
-(function($, Backbone, _, helpers, ArticleCollectionView, window, undefined) {
+;(function($, Backbone, _, helpers, ArticleCollectionView, gobal, undefined) {
 "use strict";
 
 
-var main = window.main || {};
+var main = gobal.main || {};
 
 
 // ------------------
@@ -14,9 +14,10 @@ main.ArticleItemModel = Backbone.Model.extend({
         title: ''
         ,cover_image: ''
         ,id: ''
-        ,quality: null
-        ,width: null
-        ,shard: null
+        ,quality: ''
+        ,width: ''
+        ,height: ''
+        ,shard: ''
     }
 });
 
@@ -36,19 +37,45 @@ main.ArticleListCollection = Backbone.Collection.extend({
 // -----------------
 
 main.ArticleItemView = Backbone.View.extend({
-    // the DOM element is an **<li>** tag
     tagName: 'article'
-    ,className: 'cover_article_item'
-    ,template: _.template($('#articleTemplate').html())
 
+    // Choose the correct template and className
+    // depending on the type of media file
+    ,_set_template: function() {
+        var multimedia_type = this.model.get('type');
+
+        // The template and the className depends whether the model is an
+        // image, link or video.
+        if (multimedia_type === 'image_file') {
+            this.templateId = '#image_file_template';
+            this.className = '.image_file_container';
+        } else if (multimedia_type === 'image_link') {
+            this.templateId = '#image_link_template';
+            this.className = '.image_link_container';
+        } else if (multimedia_type === 'video_link') {
+            this.templateId = '#video_link_template';
+            this.className = '.video_link_container';
+        } else {
+            this.templateId = '#image_link_template';
+            this.className = '.image_link_container';
+        }
+        this.template = _.template($(this.templateId).html());
+    }
 
     ,initialize: function() {
+        var _this = this;
+
         // Bind the actual object with the 'render' event because first need
         // update the model and after need render this view. If not bind, this
         // could be raise some error because try to render the template before
         // finish update the model.
         _.bindAll(this, 'render');
-        this.model.set({cid: this.model.cid});
+
+        // Some templates use the cid field.
+        this.model.set({
+            cid: this.model.cid
+        });
+        this._set_template();
         this.model.on('change', this.render);
     }
 
@@ -97,12 +124,11 @@ main.ArticleItemView = Backbone.View.extend({
 });
 
 
-/* CAVEAT: I set the __testmode___ enviroment variable to true in the test
-main. I do that because I don't need an instance of 
-main.ImageCollectionView during the testing. But I need that during the
-production. This is equivalent to *if __name__ == "__main__":* on python.
+/* If the jasmine object is defined mean that this module is called for
+testing. If not mean that is called for produccition. This is equivalent to
+*if __name__ == "__main__":* on python.
 */
-if (typeof __testmode__ === 'undefined') {
+if (typeof jasmine === 'undefined') {
     $(function() {
         main.articleListView = new ArticleCollectionView({
             Collection: main.ArticleListCollection,
@@ -113,6 +139,6 @@ if (typeof __testmode__ === 'undefined') {
 
 
 // Expose main to the global object
-window.main = main;
+gobal.main = main;
 
 })(jQuery, Backbone, _, helpers, ArticleCollectionView, window);
